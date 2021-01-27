81304b68debe668ff5b1cdc7ab1001eb9751edad
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2021-01-27 14:25:31 +0000

    graph

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,51 +1,55 @@
-
 const traffic = document.getElementById('traffic-chart').getContext('2d');
 const dailyCanvas = document.getElementById('daily-chart');
 const mobileCanvas = document.getElementById('mobile-chart');
 
 
 // TRAFFIC CHART
-var firstGraph = new Chart(traffic, {
-    // The type of chart we want to create
-    type: 'line',
+let trafficData = {
+    labels: ["16-22","23-29","30-5","6-12","13-19","20-26", "27-3","4-10","11-17","18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgb(226, 227, 244)',
+        borderColor: 'rgb(145, 149, 207)',
+        borderWidth: 1,
+        lineTension: 0
+    }]   
+}
 
-    // The data for our dataset
-    data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-        datasets: [{
-            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-            backgroundColor: 'rgb(226, 227, 244)',
-            borderColor: 'rgb(145, 149, 207)',
-            borderWidth: 1,
-            lineTension: 0
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration:0 
+    },
+    scales: {
+        yAxes:  [{
+            ticks: {
+                beginAtZero: true
+            }
         }]
     },
-
-    // Configuration options go here
-    options: {
-        maintainAspectRatio: true,
-        
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        },
         legend: {
             display: false
         },
-        responsive: true
+    responsive: true,
+    layout: {
+        padding: {
+            left: 50,
+            right: 0,
+            top: 0,
+            bottom: 0
+        }
     }
-})
+    }
 
-// DAILY CHART
-const dailyData = new Chart(dailyCanvas, {
-    // The type of chart we want to create 
-    type: 'bar',
+    let trafficChart = new Chart (traffic  , {
+        type:'line',
+        data: trafficData,
+        options: trafficOptions
+    });
 
-    // The data for our dataset
-    data: {
+    // BAR CHART
+
+    const dataBar = {
         labels: ["S", "M", "T", "W", "T", "F", "S"],
         datasets: [{
             label: "# of hits",
@@ -53,53 +57,68 @@ const dailyData = new Chart(dailyCanvas, {
             backgroundColor: '#7477bf',
             borderWidth: 1
         }]
-    },
+    };
 
-    // Configuration options go here
-
-    options: {
-
+    const optBar = {
         scales: {
-            yAxes: [{
+            yAxes:  [{
                 ticks: {
                     beginAtZero: true
                 }
             }]
         },
-        legend: {
-            display: false
-        }
+            legend: {
+                display: false
+            },
+            responsive: true,
+            layout: {
+                padding: 50,
 
+            }
     }
-})
+
+    let dailyChart =  new Chart(dailyCanvas, {
+        type:'bar',
+        data: dataBar,
+        options: optBar
+    });
 
 // MOBILE CHART
-const mobileData = new Chart(mobileCanvas, {
-    // The type of chart we want to create 
-    type: 'doughnut',
 
-    // The data for our dataset
-    data: {
+    const mobileData = {
         labels: ["Desktop", "Tablet", "Phones"],
-        datasets: [{
-            label: "# of Users",
-            data: ['2000', '550', '500'],
-            borderWidth: 0,
-            backgroundColor: [
-                '#7477bf',
-                '#78cf82',
-                '51b6c8'
-            ]
-        }],
-        // Configuration options go here
-        options: {
-            legend: {
+            datasets: [{
+                label: "# of Users",
+                data: ['2000', '550', '500'],
+                borderWidth: 0,
+                backgroundColor: [
+                    '#7477bf',
+                    '#78cf82',
+                    '51b6c8'
+                ]
+            }]
+    };
+
+    const mobileOptions = {
+        legend: {
+            
+                display: true,
                 position: 'right',
                 label: {
                    boxWidth: 20,
                    fontStyle: 'bold'
-                }
+                
             }
-        }
+        },
+        responsive: true,
+            layout: {
+                padding: 40,
+        
+            }
     }
-})
+
+    let mobileChart = new Chart (mobileCanvas, {
+        type:'doughnut',
+        data: mobileData,
+        options: mobileOptions
+    })
