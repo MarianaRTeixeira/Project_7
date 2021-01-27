3ed15be963e0ba5ef77251d37a7584a934b37d65
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2021-01-27 12:29:44 +0000

    css

--- scripts/chart.js
+++ scripts/chart.js
@@ -24,6 +24,7 @@ var firstGraph = new Chart(traffic, {
     // Configuration options go here
     options: {
         maintainAspectRatio: true,
+        
         scales: {
             yAxes: [{
                 ticks: {
