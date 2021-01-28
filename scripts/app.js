a07a91d59e3eed963d33fd32efd9512c1510e423
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2021-01-28 10:58:51 +0000

    js miss

--- scripts/app.js
+++ scripts/app.js
@@ -26,12 +26,13 @@ alertBanner.addEventListener ( 'click', e =>{
 // ​const​ send = ​document​.getElementById(​"send"​);
 
 // send.addEventListener(​'click'​, () => {
-//     ​// ensure user and message fields are filled out ​if​ (user.value === ​""​ && message.value === ​""​) {
-//     alert(​"Please fill out user and message fields before sending"​);
-//     } else if (user.value === ​""​ ) {
-//     alert(​"Please fill out user field before sending"​);
-//     } ​else​ ​if​ (message.value === ​""​ ) {
-//     alert(​"Please fill out message field before sending"​);
-//     } ​else​ {
-//     alert(​`Message successfully sent to: ${user.value}`​); }
-//     });
+//     if(user.value === "" && message.value === "") {
+//         alert("please fill out user field and message fields before sending");
+//     } else if (user.value === ""){
+//         alert("please fill out user field");
+//     } else if (message.value === ""){
+//         alert("please fill out message field");
+//     } else {
+//         alert (`Message successfully sent to:`)
+//     }
+//     ​})
