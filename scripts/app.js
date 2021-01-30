a6844a1e9a9c9977fe36d4db2044f87dc88e5a95
MarianaRTeixiera <marianamrochateixeira@gmail.com>
2021-01-30 12:21:32 +0000

    formating and names

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const alertBanner = document.getElementById("alert");
 
 alertBanner.innerHTML =
-`
+    `
 <div class="alert-banner">
       <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
@@ -12,27 +12,27 @@ to complete</p>
 `
 
 
-alertBanner.addEventListener ( 'click', e =>{
+alertBanner.addEventListener('click', e => {
     const element = e.target;
-    if (element.classList.contains("alert-banner-close")){
-            alertBanner.style.display = 'none';
-            
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = 'none';
+
     }
-}); 
+});
 
 
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
-send.addEventListener( 'click', () => {
-  if(user.value === "" && message.value === "") {
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === "") {
         alert("please fill out user field and message fields before sending");
-    } else if (user.value === ""){
+    } else if (user.value === "") {
         alert("please fill out user field");
-    } else if (message.value === ""){
+    } else if (message.value === "") {
         alert("please fill out message field");
     } else {
-        alert (`Message successfully sent to:`)
+        alert(`Message successfully sent to:`)
     }
-    });
+});
